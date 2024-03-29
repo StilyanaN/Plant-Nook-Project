8f929adc5dcc1b05d6243a1a963fe54283a351ab
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-03-30 01:13:24 +0200

    Add routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
+
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '',pathMatch: 'full',redirectTo:'/home'},
+  {path:'home',  component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  {
+    path: 'users',
+    loadChildren: () => import('./user/user.module').then(u => u.UserModule)
+  },
+  { path: '**', redirectTo: '/404' },
+  { path: '404', component: ErrorComponent },
+
+
 ];
 
 @NgModule({
