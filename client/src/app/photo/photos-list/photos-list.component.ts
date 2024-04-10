76a49081619c10e89c2dbae0bd8c3f22e596402a
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-04-10 03:47:10 +0300

    Update all posts page

--- client/src/app/photo/photos-list/photos-list.component.ts
+++ client/src/app/photo/photos-list/photos-list.component.ts
@@ -1,39 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/api.service';
-import { Photo } from 'src/app/types/photo';
-import { UserService } from 'src/app/user/user.service';
+import { ApiService } from '../../api.service';
+import { Photo } from '../../types/photo';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-photos-list',
   templateUrl: './photos-list.component.html',
   styleUrls: ['./photos-list.component.css']
 })
+export class PhotosListComponent implements OnInit {
+  photos: Photo[] | null = [];
+  isLoading: boolean = true;
+  hasPosts: boolean = true;
 
-  export class PhotosListComponent implements OnInit {
+  constructor(private api: ApiService, private userService: UserService) {}
 
-    photos: Photo[] | null = [];
-    isLoading: boolean = true;
-     hasPosts: boolean = true;
-  
-    constructor(private api: ApiService, private userService:UserService) {}
-
-    get isLoggedIn(): boolean {
-      return this.userService.isLogged;
-    }
-  
-    ngOnInit(): void {
-
-      this.api.getPhotos().subscribe((photos) => {
-        this.photos = photos;
-
-        if(this.photos && this.photos.length === 0){
-          this.hasPosts = false;
-        }
-  
-        setTimeout(() => {
-          this.isLoading = false;
-        }, 3000);
-      });
-    }
-  
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
   }
+
+  ngOnInit(): void {
+    this.api.getPhotos().subscribe((photos) => {
+      this.photos = photos;
+
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 2000);
+
+
+      if (this.photos.length === 0) {
+        this.hasPosts = false;
+      }
+    });
+  }
+
+  getTotalLikes(photo: Photo): number {
+    return photo.likes ? photo.likes.length : 0;
+  }
+}
