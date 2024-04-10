dead618946124d1f5e8b4a206a8bc67d075f5076
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-04-10 03:43:52 +0300

    Update photo interfaces

--- client/src/app/types/photo.ts
+++ client/src/app/types/photo.ts
@@ -1,12 +1,27 @@
-import { User } from "./user";
+import { User } from './user';
 
 export interface Photo {
-    title: string;
-    image: string;
-    description: string;
-    category: string;
-    likes: string[];
-    owner: User; 
-    photoId: string;
-   
+  title?: string;
+  image?: string;
+  description?: string;
+  category?: string;
+  _id?: string;
+  like?: { liked: boolean; likesCount: number };
+  likes?: { userId: string }[];
+  owner?: User;
+  likesCount?:number;
+  postId?: string;
+  userId?: User;
+}
+
+export interface PhotoCreate {
+  title: string;
+  description: string;
+  category: string;
+  image: string;
+}
+
+export interface Like {
+  liked: boolean;
+  likesCount: number;
 }
