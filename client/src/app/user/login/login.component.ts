44dbc906231cb03124ba4c4dc8464c4a41635fa9
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-04-10 03:37:41 +0300

    Change login component to template-driven

--- client/src/app/user/login/login.component.ts
+++ client/src/app/user/login/login.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
-import { FormBuilder, Validators } from '@angular/forms';
-import {  UserForLogin } from 'src/app/types/user';
+import { NgForm } from '@angular/forms';
 import { EMAIL_DOMAINS } from 'src/app/shared/constants';
-import { emailValidator } from 'src/app/shared/utils/email-validator';
 
 @Component({
   selector: 'app-login',
@@ -12,36 +10,18 @@ import { emailValidator } from 'src/app/shared/utils/email-validator';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  
-  formLogin = this.fb.group({
-    email: ['', [Validators.required, emailValidator(EMAIL_DOMAINS)]],
-    password: ['', [Validators.required, Validators.minLength(5)]],
-  });
+  domains = EMAIL_DOMAINS;
+  constructor(private userService: UserService, private router: Router) {}
 
-  constructor(
-    private fb: FormBuilder,
-    private userService: UserService,
-    private router: Router
-  ) {}
-
-  login(): void {
-    if (!this.formLogin.valid) {
+  login(form: NgForm) {
+    if (form.invalid) {
       return;
     }
 
-    const formData = this.formLogin.value
-        this.userService
-          .login(formData as UserForLogin)
-          .subscribe((data) => {
-            if (data?.error) {
-              this.formLogin.reset();
-              return;
-            }
-            this.router.navigate(['/home']);
-          });
-        }
+    const { email, password } = form.value;
+
+    this.userService.login(email, password).subscribe(() => {
+      this.router.navigate(['/posts']);
+    });
   }
-
-
- 
-
+}
